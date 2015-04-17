d2f6379f438e5865ffd76ac25ebdb06ce66f109f
Tom Mertz <tom.mertz@verizon.net>
2015-04-17 10:25:20 -0700

    Add spec for pepperoni pizza

--- spec/specs.js
+++ spec/specs.js
@@ -3,9 +3,16 @@ describe("Pizza", function() {
     it("should give a price of 10 if a customer orders a cheese pizza", function() {
 
       var pizza = Object.create(Pizza);
-      pizza.init();
+      pizza.init(1);
 
       expect(pizza.cost()).to.equal(10);
     });
+
+    it("should give a price of 12 for a pepperoni pizza", function() {
+      var pizza = Object.create(Pizza);
+      pizza.init(2);
+
+      expect(pizza.cost()).to.equal(12);
+    })
   });
 });
