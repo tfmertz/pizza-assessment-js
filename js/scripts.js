9922341e5a19f26e57d62c2300df8ea386e034a1
Tom Mertz <tom.mertz@verizon.net>
2015-04-17 15:46:41 -0700

    Add ability for users to input multiple types of pizza

--- js/scripts.js
+++ js/scripts.js
@@ -42,35 +42,63 @@ $(function() {
 
     //grab the values from the form
     var patronName = $("#name").val();
-    var pizzaType = parseInt($("#pizza-type").val());
 
-    //create our pizza object
-    var pizza = Object.create(Pizza);
-    pizza.init(pizzaType);
-    var cost = pizza.cost();
+    //use a jquery each to create pizza objects for each pizza types
+    var pizzaArray = [];
+    var totalCost = 0;
+    var validInput = true;
+
+    $(".pizza-type").each(function() {
+      //create a pizza object
+      var pizza = Object.create(Pizza);
+      //initialize it with the value from this current select
+      var pizzaNumber = parseInt($(this).val());
+      pizza.init(pizzaNumber);
+      //add its cost to the total if it is a valid pizza
+      pizzaCost = pizza.cost();
+
+      if(typeof(pizzaCost) === "number") {
+
+        pizzaArray.push('<li>' + 1 + ' x ' + pizza.name + '</li>');
+        totalCost += pizza.cost();
+        console.log(pizzaArray);
+      } else {
+        //if not make a flag to show the error and skip the rest
+        validInput = false;
+        return false;
+      }
+    });
 
     //if we returned a valid cost number then add it to our placed orders
-    if(typeof(cost) === "number") {
+    if(validInput) {
 
       //add an order div to append all the order details to
       var $orderDiv = $('<div class="order"></div>');
       //create title text to add to the div with the delete button
       var orderTitle = '<p>Order Name: ' + patronName + '<span title="Cancel Order" class="order__cancel-order glyphicon glyphicon-remove"></span></p>';
       //create total text to add to bottom of order div
-      var orderTotal = '<p>Amount Total: $' + cost + '</p>';
+      var orderTotal = '<p>Amount Total: $' + totalCost + '</p>';
       //create orderList element to append things to
       var $orderList = $('<ul class="order__order-details"></ul>');
-      //append all details to the list
-      $orderList.append('<li>' + 1 + ' x ' + pizza.name + '</li>')
+      //append all pizzas from our array to the list
+      $.each(pizzaArray, function(index, element) {
+        console.log(element);
+        $orderList.append(element);
+      });
 
+      //append all our children into the order div
       $orderDiv.append(orderTitle);
       $orderDiv.append($orderList);
       $orderDiv.append(orderTotal);
 
+      //append the order div to the list of our orders
       $("#order-list").append($orderDiv);
 
+      //add click listener to cancel an order
       $(".order__cancel-order").last().click(function() {
+        //remove the order div 2 parents up
         $(this).parent().parent().remove();
+        //if there aren't any more orders in the #placed-orders div, then hide it
         if($("#order-list").children().length === 0) {
           $("#placed-orders").css("visibility", "hidden");
         }
@@ -79,13 +107,11 @@ $(function() {
       $("#error").hide();
       $("#placed-orders").css("visibility", "visible");
     }
-    else
-    {
+    else {
       $("#error").show();
     }
 
-
-  });
+  }); //end submit
 
   //when the user clicks the add pizza button
   $("#add-pizza").click(function() {
